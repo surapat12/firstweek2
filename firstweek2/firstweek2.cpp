5348b4f0fb82b48e1068da4f2f416fc61e5e224e
iamsurapat12 <iamsurapat12@outlook.co.th>
2020-08-11 15:11:43 +0700

    Add project files.

---
+++ firstweek2/firstweek2.cpp
@@ -0,0 +1,37 @@
+﻿#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+	float Amount, Principal, Interest, Years, x ;
+	printf("Amount of compound interest");
+	printf("\n\n");
+	printf("Principal = ");
+	scanf_s("%f", &Principal);
+	printf("\n\n");
+	printf("Interest = ");
+	scanf_s("%f", &Interest);
+	printf("\n\n");
+	printf("Years = ");
+	scanf_s("%f", &Years);
+	x = 1 + (Interest / 100);
+	Amount = Principal * pow(x, Years);
+	printf("Amount = %.2f", Amount);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
